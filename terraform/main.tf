b6d100720d219df9ab1d2ca390c6e28acf6a70b4
akrishnakumar-splunk <akrishnakumar@splunk.com>
2019-05-02 14:19:18 +0530

    02-05 Changing The defs for cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,25 +13,43 @@ resource "google_dns_record_set" "dns" {
 
 data "google_compute_zones" "available" {}
 
-data "google_container_engine_versions" "gke_version" {}
+data "google_container_engine_versions" "gke_version" {
+    zone = "${data.google_compute_zones.available.names[0]}"
+}
 
 resource "google_container_cluster" "primary" {
   name               = "${var.cluster_name}"
   zone               = "${data.google_compute_zones.available.names[0]}"
   node_version       = "${data.google_container_engine_versions.gke_version.latest_node_version}"
   min_master_version = "${data.google_container_engine_versions.gke_version.latest_master_version}"
-  initial_node_count = 2
-
-  additional_zones = [
-    "${data.google_compute_zones.available.names[1]}",
-    "${data.google_compute_zones.available.names[2]}",
-    "${data.google_compute_zones.available.names[3]}",
-  ]
+  initial_node_count = 1
+  remove_default_node_pool = true
 
   node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  cluster    = "${google_container_cluster.primary.name}"
+  zone       = "${data.google_compute_zones.available.names[0]}"
+  node_count = 2
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    metadata {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,13 @@
 provider "google" {
-  credentials = "${file("terraform-user.json")}"
+  credentials = "${file("credentials.json")}"
   project = "${var.project}"
   region  = "${var.region}"
-  zone = "${var.region}-f"
   version = "~> 1.20"
 }
 
 provider "google-beta" {
-  credentials = "${file("terraform-user.json")}"
+  credentials = "${file("credentials.json")}"
   project = "${var.project}"
   region  = "${var.region}"
-  zone = "${var.region}-f"
   version = "~> v2.1.0"
 }
