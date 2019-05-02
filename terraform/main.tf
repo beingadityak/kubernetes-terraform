069b20ea0cb9f09aea1c58c4dd159b438b9367cb
akrishnakumar-splunk <akrishnakumar@splunk.com>
2019-05-02 15:56:23 +0530

    02-05 - Configured Project to run on Cloud shell

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,6 +31,7 @@ resource "google_container_cluster" "primary" {
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
 }
@@ -52,6 +53,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
 }
