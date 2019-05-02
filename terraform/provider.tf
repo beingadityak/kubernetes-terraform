5927d558ca39731687c1335b7ee9c3464dc1b89f
akrishnakumar-splunk <akrishnakumar@splunk.com>
2019-05-02 13:09:49 +0530

    02-05 - Adding creds details (for using Svc accounts)

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,15 @@
 provider "google" {
+  credentials = "${file("terraform-user.json")}"
   project = "${var.project}"
   region  = "${var.region}"
+  zone = "${var.region}-f"
   version = "~> 1.20"
 }
 
 provider "google-beta" {
+  credentials = "${file("terraform-user.json")}"
   project = "${var.project}"
   region  = "${var.region}"
+  zone = "${var.region}-f"
   version = "~> v2.1.0"
 }
