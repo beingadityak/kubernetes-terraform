6171c6d1cee22d2f5c9e2b2e867d5d56bc24831e
akrishnakumar-splunk <akrishnakumar@splunk.com>
2019-05-02 08:25:52 +0530

    02-05 - Updating manifests as per file name

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ var os = require('os');
 var hostname = os.hostname();
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello! Welcome to this Application!\n" + "Hostname is : "+ hostname);
+  response.end(" PG User is: " + process.env.POSTGRES_USER + "\n PG Host is :"+ process.env.POSTGRES_HOST +"\n PG Port is :"+ process.env.POSTGRES_PORT +"\n PG Database: "+ process.env.POSTGRES_DB +"\n" + "Hello! Welcome to this Application!\n" + "Hostname is : "+ hostname);
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
