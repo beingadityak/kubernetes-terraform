9bd66d8f320b86c34c77cdd7846bffc9b6fcff42
akrishnakumar-splunk <akrishnakumar@splunk.com>
2019-05-02 16:49:12 +0530

    02-05 Fully configured! Script left for joining all these together

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,14 +11,15 @@ resource "google_dns_record_set" "dns" {
   rrdatas = ["${google_compute_global_address.ingress_ip.address}"]
 }
 
+data "google_compute_zones" "available" {}
 
 data "google_container_engine_versions" "gke_version" {
-    zone = "us-central1-a"
+    zone = "${data.google_compute_zones.available.names[0]}"
 }
 
 resource "google_container_cluster" "primary" {
   name               = "${var.cluster_name}"
-  zone               = "us-central1-a"
+  zone               = "${data.google_compute_zones.available.names[0]}"
   node_version       = "${data.google_container_engine_versions.gke_version.latest_node_version}"
   min_master_version = "${data.google_container_engine_versions.gke_version.latest_master_version}"
   initial_node_count = 1
@@ -27,7 +28,7 @@ resource "google_container_cluster" "primary" {
   node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
       "https://www.googleapis.com/auth/cloud-platform",
@@ -35,14 +36,13 @@ resource "google_container_cluster" "primary" {
   }
 }
 
-resource "google_container_node_pool" "primary_preemptible_nodes" {
+resource "google_container_node_pool" "primary_nodes" {
   name       = "my-node-pool"
   cluster    = "${google_container_cluster.primary.name}"
-  zone       = "us-central1-a"
+  zone       = "${data.google_compute_zones.available.names[0]}"
   node_count = 2
 
   node_config {
-    preemptible  = true
     machine_type = "n1-standard-1"
 
     metadata {
@@ -53,7 +53,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
       "https://www.googleapis.com/auth/cloud-platform",
-      "https://www.googleapis.com/auth/devstorage.read_write",
     ]
   }
 }
